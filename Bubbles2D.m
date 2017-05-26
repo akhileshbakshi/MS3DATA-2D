1c9020ad11b5ca7c514a4a932c92fe7cb88568e6
Akhilesh Bakshi <abakshi@mit.edu>
2017-05-26 17:19:52 -0400

    Update Bubbles2D.m

--- Bubbles2D.m
+++ Bubbles2D.m
@@ -1,5 +1,6 @@
 % variables are tailored for 'bubblestats2D.txt' and 'geometry.xlsx' provided 
 % Major Edit 1: func_bubblevelocity has been edited to include minbubbledia_vel, ylim1, ylim2 for better linking 
+% Major Edit 2: option lagrangetracking has been added where bubbles are individually tracked through frames 
 
 clear all; clc; 
 
@@ -33,7 +34,13 @@ ylim1 = 0;              % min y for postprocessing
 ylim2 = ycutoff2;       % max y for postprocessing
 rlim1 = 0;              % min x for postprocessing
 rlim2 = D;              % max x for postprocessing
-minbubbledia_vel = 0.02;% discard small bubbles to improve bubble linking 
+minbubbledia_vel = 0.01;% discard very small bubbles for bubble linking
+diaratio = 1.1;         % maximum permissible ratio of bubble dia for linking  
+dmax = 0.05;            % maximum permissible distance traveled by bubble in one time-step  
+tolerance  = 0.0;       % minimum permissible bubble y-velocity = -tolerance x time-step 
+lagrangetracking = 1;   % (recommended) set 1 to turn on lagrangian tracking of bubbles 
+                        % for globaltracking (lagrangetracking=0), consider increasing minbubledia_vel
+                        % and choosing [ylim1, ylim2] to exclude small bubbles may improve linking 
 
 % 6. Statistics for average computations 
 nbinsax = 10;           % # bins for axial statistics between [ylim1, ylim2]
@@ -43,9 +50,8 @@ nbinsrad = 4;           % # bins for radial/lateral statistics between [rlim1, r
 [nframes, bubblepropertiestotal] = func_bubbledetection(bubblefile, xsmooth, ysmooth, epgcutoff, epgbubble, mincordlength, minCSlength, minbubbledia, nframes, ycutoff1, ycutoff2);
 % bubblepropertiestotal = [frame#, xmean, ymean, bubble-dia, xmin, xmax, ymin, ymax, AR]
 
-bubblepropertiestotal = func_bubblevelocity(bubblepropertiestotal, tstep, D, minbubbledia_vel, ylim1, ylim2); 
+bubblepropertiestotal = func_bubblevelocity(bubblepropertiestotal, tstep, minbubbledia_vel, ylim1, ylim2, lagrangetracking, diaratio, dmax, tolerance); 
 % bubblepropertiestotal = [frame#, xmean, ymean, bubble-dia, xmin, xmax, ymin, ymax, AR, vx, vy]
-% increasing minbubledia_vel and choosing [ylim1, ylim2] to exclude small bubbles may improve linking 
 
 % ----------------------------------------------------------------
 % sample for computing average statistics 
